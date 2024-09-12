da40bdd36fc8776dfba0eaeafbdaf526f9bbd617
shashikant010 <shashikantyadav0503@gmail.com>
2024-09-12 20:16:02 +0530

    complete a three js practice project in vanilla js

--- project1/main.js
+++ project1/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 //1. create a scene
 const scene = new THREE.Scene();
-scene.background=new THREE.Color("#F0F0F0");
+scene.background=new THREE.Color("#000");
 
 //add a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,13 +9,13 @@ camera.position.z=5;
 
 //create and add a cube object
 const geometry = new THREE.BoxGeometry();
-const material =  new THREE.MeshLambertMaterial({color:"#468585",emissive:"#468585"});
+const material =  new THREE.MeshLambertMaterial({color:"#421554",emissive:"#421585"});
 const cube = new THREE.Mesh(geometry,material);
 scene.add(cube);
 
 //add lighting
 const light = new THREE.DirectionalLight(0x9CDBA6,10);
-light.position.set(1,1,1);
+light.position.set(1,43,1);
 scene.add(light)
 
 //setup the renderer
--- project2/main.js
+++ project2/main.js
@@ -1,24 +1,70 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector("#canvas");
 
-setupCounter(document.querySelector('#counter'))
+//scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#f0f0f0");
+
+
+//camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 7;
+
+//object
+const geomentry = new THREE.DodecahedronGeometry()
+
+const material = new THREE.MeshBasicMaterial({color:"#468585"});
+
+const Dodecahedron = new THREE.Mesh(geomentry,material  )
+
+Dodecahedron.position.set(-1,0,0)
+
+const boxGeometry = new THREE.BoxGeometry({width:2,height:0.2,depth:2})
+const boxMaterial = new THREE.MeshBasicMaterial({color:"#5e5e5e"})
+const box = new THREE.Mesh(boxGeometry,boxMaterial)
+//add position to box
+box.position.set(0,1,0)
+scene.add(Dodecahedron)
+scene.add(box)
+
+//light
+const light = new THREE.SpotLight(0x006769, 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+//renderer
+const renderer = new THREE.WebGLRenderer({  canvas });  
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+//add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+//add animation
+const animate = () => {
+  requestAnimationFrame(animate);
+  Dodecahedron.rotation.x += 0.01;
+  Dodecahedron.rotation.y += 0.01;
+  controls.update();
+  renderer.render(scene, camera);
+};
+animate();
+
+//handle window resizing
+window.addEventListener("resize", () => {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+})
